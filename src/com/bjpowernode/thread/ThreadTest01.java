6a974288b0dcedf73d958850c8426cbbdb1670e3
travelmlc <2728604863@qq.com>
2020-03-29 12:00:57 +0800

    01

--- src/com/bjpowernode/thread/ThreadTest01.java
+++ src/com/bjpowernode/thread/ThreadTest01.java
@@ -1,12 +1,7 @@
 package com.bjpowernode.thread;
 
 /**
- * ClassName:ThreadTest01
- * Package:com.bjpowernode.thread
- * Description:
  *
- * @Date:2020/3/28 17:20
- * @Author:mlc17607163664@163.com
  */
 public class ThreadTest01 {
 
